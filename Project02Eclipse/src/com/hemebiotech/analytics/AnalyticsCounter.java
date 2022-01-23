7aeb0213546705218ad1ed1ce71c5aa91e55fbe1
raranguren <ricaragas@hotmail.com>
2022-01-23 01:03:10 +0100

    split original solution into 4 methods; running AnalyticsCounter as an instanced object

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,39 +3,66 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	
-	public static void main(String[] args) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+	private int headacheCount;
+	private int rashCount;
+	private int pupilCount;
 
-			line = reader.readLine();	// get another symptom
-		}
-
-		reader.close();
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("~ pupils: " + pupilCount + "\n");
-		writer.close();
+	public void run() {
+		readSymptomsFromFile();
+		countSymptoms();
+		sortResults();
+		writeResultsToFile();
 	}
+
+	private void readSymptomsFromFile() {
+
+		try {
+			BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+			String line;
+			line = reader.readLine();
+			while (line != null) {
+				System.out.println("symptom from file: " + line);
+				if (line.equals("headache")) {
+					headacheCount++;
+				}
+				else if (line.equals("rash")) {
+					rashCount++;
+				}
+				else if (line.contains("pupils")) {
+					pupilCount++;
+				}
+
+				line = reader.readLine();	// get another symptom
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void countSymptoms() {
+		// TODO split reader into read+count
+	}
+
+	private void sortResults() {
+		// TODO sort results
+	}
+
+	private void writeResultsToFile() {
+		try {
+			FileWriter writer = new FileWriter ("result.out");
+			writer.write("headache: " + headacheCount + "\n");
+			writer.write("rash: " + rashCount + "\n");
+			writer.write("~ pupils: " + pupilCount + "\n");
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -0,0 +1,10 @@
+package com.hemebiotech.analytics;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new AnalyticsCounter().run();
+
+    }
+}
